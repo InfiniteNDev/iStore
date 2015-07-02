fcedee00aa3289f00ca819ecc55c6f4c5ede0c42
InfiniteN <liubingyao@gmail.com>
2015-07-02 08:40:39 +0100

    add views and routes

---
+++ DOCUMENT.md
@@ -0,0 +1,61 @@
+# iStroe
+An eCommerce app based on Laravel.
+
+Author: InfiniteN [Email](mailto:liubingyao@gmail.com)
+
+# Depdencies
+* npm
+* Composer
+* Grunt
+  * grunt-contrib-concat
+  * grunt-contrib-sass
+  * grunt-contrib-uglify
+  * grunt-contrib-watch
+  * grunt-phpunit
+  * load-grunt-tasks
+* Bower
+  * AngularJs
+  * Bootstrap-sass
+  * Modernizr
+  * jQuery
+
+# Trello
+[Trello](https://trello.com/b/hiE0qQkV)
+
+# Page
+* Frontend
+  * homepage
+  * products
+    * show all products
+    * filter
+  * single product
+    * show single product's detail information
+  * articles
+    * show all articles
+    * filter
+  * single article
+    * show single article's whole content
+  * sign
+    * sign up or log in
+  * cart
+    * show cart
+  * checkout
+    * show checkout information
+* Backend
+  * login
+    * log in to backend
+  * homepage
+  * products
+    * show all products, CRUD
+  * single product
+    * show single products, CRUD
+  * articles
+    * show all articles, CRUD
+  * single article
+    * show single article, CRUD
+  * orders
+    * show all orders, CRUD
+  * single order
+    * show single order, CRUD
+  * users
+    * show all users, CRUD
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,102 @@
 |
 */
 
+// frontend
+// frontend's homepage
 Route::get('/', function () {
-    return view('welcome');
+  return view('index');
 });
 
-Route::get('/index', function () {
-    return view('index');
+// products page: show all products
+Route::get('/products', function () {
+  return view('products');
+});
+
+// product's detail page: show single product
+// fix: enter with id
+Route::get('/product', function () {
+  return view('product');
+});
+
+// articles page: show all articles
+Route::get('/articles', function () {
+  return view('articles');
+});
+
+// article's detial page: show single article
+// fix: enter with id
+Route::get('/article', function () {
+  return view('article');
+});
+
+// sign page: sign up or log in
+Route::get('/account', function () {
+  return view('account');
+});
+
+// account page: show user's information
+Route::get('/account', function () {
+  return view('account');
+});
+
+// cart page: show cart
+Route::get('/cart', function () {
+  return view('cart');
+});
+
+// checkout page: show checkout
+Route::get('/checkout', function () {
+  return view('checkout');
+});
+
+// backend
+// fix: only admin user could enter
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
+  // admin login
+  Route::get('/', function () {
+    return view('admin/login');
+  });
+
+  // admin homepage
+  Route::get('/', function () {
+    return view('admin/admin');
+  });
+
+  // admin products page: show all products, CRUD
+  Route::get('/products', function () {
+    return view('admin/products');
+  });
+
+  // admin single product page: show single product, CRUD
+  // fix: with id
+  Route::get('/product', function () {
+    return view('admin/product');
+  });
+
+  // admin articles page: show all articles, CRUD
+  Route::get('/articles', function () {
+    return view('admin/articles');
+  });
+
+  // admin single article page: show single article, CRUD
+  // fix: with id
+  Route::get('/article', function () {
+    return view('admin/article');
+  });
+
+  // admin orders page: show all orders
+  Route::get('/orders', function () {
+    return view('admin/orders');
+  });
+
+  // admin single order page: show single order
+  // fix: with id
+  Route::get('/order', function () {
+    return view('admin/order');
+  });
+
+  // admin users page: show all users
+  Route::get('/users', function () {
+    return view('admin/users');
+  });
 });
