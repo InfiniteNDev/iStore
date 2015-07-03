e610e0c53277b60470dc79892366d6bc121b25a9
InfiniteN <liubingyao@gmail.com>
2015-07-03 08:54:05 +0100

    navbar link change active with url

--- resources/views/layouts/backPartials/header.blade.php
+++ resources/views/layouts/backPartials/header.blade.php
@@ -15,18 +15,24 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="{{ URL::to('admin') }}">Home <span class="sr-only">(current)</span></a></li>
-          @if (Auth::check())
-            <li><a href="{{ URL::to('admin/products') }}">Products</a></li>
-            <li><a href="{{ URL::to('admin/articles') }}">Articles</a></li>
-            <li><a href="{{ URL::to('admin/users') }}">Users</a></li>
-          @endif
+          <li class='{{ Request::is( 'admin') ? 'active' : '' }}'>
+            <a href="{{ URL::to('admin') }}">Home</a>
+          </li>
+          <li class='{{ Request::is( 'admin/products') ? 'active' : '' }}'>
+            <a href="{{ URL::to('admin/products') }}">Products</a>
+            </li>
+          <li class='{{ Request::is( 'admin/articles') ? 'active' : '' }}'>
+            <a href="{{ URL::to('admin/articles') }}">Articles</a>
+          </li>
+          <li class='{{ Request::is( 'admin/users') ? 'active' : '' }}'>
+            <a href="{{ URL::to('admin/users') }}">Users</a>
+          </li>
         </ul>
 
         <ul class="nav navbar-nav navbar-right">
           <li><a href="{{ URL::to('/') }}">Back to FrontEnd</a></li>
           @if (Auth::check())
-            <li><a href="{{ URL::to('admin/logout') }}">Logout</a></li>
+          <li><a href="{{ URL::to('admin/logout') }}">Logout</a></li>
           @endif
         </ul>
 
